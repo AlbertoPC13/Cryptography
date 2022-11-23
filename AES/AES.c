18e832993aceeb0e0a405af9b6d90a4596d0f35a
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2022-11-23 00:00:01 -0600

    AES Cipher algorithm uploaded

--- AES/AES.c
+++ AES/AES.c
@@ -18,24 +18,23 @@ unsigned char s[256] = {
     0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
     0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
     0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
-};
+    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16};
 
-unsigned char ** keyScheduleCreation(void)
+unsigned char **keyScheduleCreation(void)
 {
-    unsigned char ** w;
-    short n = Nb*(Nr+1);
-    w = (unsigned char **)malloc(n*sizeof(unsigned char *));
-    
+    unsigned char **w;
+    short n = Nb * (Nr + 1);
+    w = (unsigned char **)malloc(n * sizeof(unsigned char *));
+
     for (short i = 0; i < n; i++)
-        w[i] = (unsigned char *) calloc(2,2);
+        w[i] = (unsigned char *)calloc(2, 2);
 
     return w;
 }
 
-unsigned char * word(unsigned char w0,unsigned char w1,unsigned char w2,unsigned char w3)
+unsigned char *word(unsigned char w0, unsigned char w1, unsigned char w2, unsigned char w3)
 {
-    unsigned char *w = (unsigned char *) malloc(4*sizeof(unsigned char)); 
+    unsigned char *w = (unsigned char *)malloc(4 * sizeof(unsigned char));
 
     w[0] = w0;
     w[1] = w1;
@@ -45,21 +44,21 @@ unsigned char * word(unsigned char w0,unsigned char w1,unsigned char w2,unsigned
     return w;
 }
 
-unsigned char * rotWord(unsigned char *w)
+unsigned char *rotWord(unsigned char *w)
 {
-    unsigned char *temp = (unsigned char *) malloc(4*sizeof(unsigned char));
+    unsigned char *temp = (unsigned char *)malloc(4 * sizeof(unsigned char));
 
     temp[0] = w[1];
     temp[1] = w[2];
     temp[2] = w[3];
-    temp[3] = w[0];    
+    temp[3] = w[0];
 
     return temp;
 }
 
-unsigned char * subWord(unsigned char *w)
+unsigned char *subWord(unsigned char *w)
 {
-    unsigned char *temp = (unsigned char *) malloc(4*sizeof(unsigned char));
+    unsigned char *temp = (unsigned char *)malloc(4 * sizeof(unsigned char));
 
     temp[0] = s[w[0]];
     temp[1] = s[w[1]];
@@ -69,45 +68,138 @@ unsigned char * subWord(unsigned char *w)
     return temp;
 }
 
-unsigned char * word_XOR(unsigned char *a, unsigned char *b)
+unsigned char *word_XOR(unsigned char *a, unsigned char *b)
 {
-    unsigned char *temp = (unsigned char *) malloc(4*sizeof(unsigned char));
+    unsigned char *temp = (unsigned char *)malloc(4 * sizeof(unsigned char));
 
-    temp[0] = a[0] ^ b [0];
-    temp[1] = a[1] ^ b [1];
-    temp[2] = a[2] ^ b [2];
-    temp[3] = a[3] ^ b [3];
+    temp[0] = a[0] ^ b[0];
+    temp[1] = a[1] ^ b[1];
+    temp[2] = a[2] ^ b[2];
+    temp[3] = a[3] ^ b[3];
 
     return temp;
 }
 
 void keyExpansion(unsigned char *key, unsigned char **w)
 {
-    unsigned char *temp = (unsigned char *) malloc(4*sizeof(unsigned char));
-    
+    unsigned char *temp = (unsigned char *)malloc(4 * sizeof(unsigned char));
+
     short i = 0;
 
     while (i < Nk)
     {
         w[i] = word(key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]);
-        i++; 
-    }       
+        i++;
+    }
 
     short wi = Nb * (Nr + 1);
     while (i < wi)
     {
-        temp = word(w[i - 1][0],w[i - 1][1],w[i - 1][2],w[i - 1][3]);
-        
-        if(i % Nk == 0)
+        temp = word(w[i - 1][0], w[i - 1][1], w[i - 1][2], w[i - 1][3]);
+
+        if (i % Nk == 0)
         {
             temp = subWord(rotWord(temp));
-            XOR(temp,temp,word(rcon[i / Nk],0x00,0x00,0x00));
+            XOR(temp, temp, word(rcon[i / Nk], 0x00, 0x00, 0x00));
         }
-        else if( Nk > 6 && i % Nk == 4)
+        else if (Nk > 6 && i % Nk == 4)
             temp = subWord(temp);
-        
-        XOR(w[i],w[i - Nk],temp);
 
-        i++; 
+        XOR(w[i], w[i - Nk], temp);
+
+        i++;
     }
+}
+
+unsigned char *Cipher(unsigned char *in, unsigned char **w)
+{
+    unsigned char **state = StateCreation(in);
+
+    AddRoundKey(state, w, 0);
+
+    for (int round = 1; round <= Nr; round++)
+    {
+        subBytes(state);
+        shiftRows(state);
+        // mixColumns(state);
+        AddRoundKey(state, w, round * Nb);
+    }
+
+    subBytes(state);
+    shiftRows(state);
+    AddRoundKey(state, w, Nr * Nb);
+
+    // out = state;
+}
+
+void AddRoundKey(unsigned char **state, unsigned char **w, short index)
+{
+    for (short i = 0; i < Nb; i++)
+        for (short j = 0; j < 4; j++)
+            state[j][i] ^= w[index + i][j];
+}
+
+void subBytes(unsigned char **state)
+{
+    for (short i = 0; i < Nb; i++)
+        for (short j = 0; j < 4; j++)
+            state[i][j] = s[state[i][j]];
+}
+
+void shiftRows(unsigned char **state)
+{
+    unsigned char *temp = (unsigned char *)malloc(4 * sizeof(unsigned char));
+
+    temp[0] = state[1][0];
+    temp[1] = state[1][1];
+    temp[2] = state[1][2];
+    temp[3] = state[1][3];
+
+    state[1][0] = temp[1];
+    state[1][1] = temp[2];
+    state[1][2] = temp[3];
+    state[1][3] = temp[0];
+
+    temp[0] = state[2][0];
+    temp[1] = state[2][1];
+    temp[2] = state[2][2];
+    temp[3] = state[2][3];
+
+    state[2][0] = temp[2];
+    state[2][1] = temp[3];
+    state[2][2] = temp[0];
+    state[2][3] = temp[1];
+
+    temp[0] = state[3][0];
+    temp[1] = state[3][1];
+    temp[2] = state[3][2];
+    temp[3] = state[3][3];
+
+    state[3][0] = temp[3];
+    state[3][1] = temp[2];
+    state[3][2] = temp[1];
+    state[3][3] = temp[0];
+}
+
+unsigned char **StateCreation(unsigned char *in)
+{
+    unsigned char **State;
+    short n = Nb;
+    State = (unsigned char **)malloc(n * sizeof(unsigned char *));
+
+    for (short i = 0; i < n; i++)
+        State[i] = (unsigned char *)calloc(2, 2);
+
+    short k = 0;
+
+    for (short i = 0; i < 4; i++)
+    {
+        for (short j = 0; j < 4; j++)
+        {
+            State[j][i] = in[k];
+            k++;
+        }
+    }
+
+    return State;
 }
--- AES/AES.h
+++ AES/AES.h
@@ -1,16 +1,21 @@
 #include<stdlib.h>
 
 # define Nb 4 // Block size
-//# define Nk 4 // Key Length 128
-# define Nk 6 // Key Length 192
-//# define Nr 10 // Number of rounds 128
-# define Nr 12 // Number of rounds 192
+# define Nk 4 // Key Length 128
+//# define Nk 6 // Key Length 192
+# define Nr 10 // Number of rounds 128
+//# define Nr 12 // Number of rounds 192
 
 #define copy(A,B) for(int it = 0; it < 4; it++) A[it] = B[it]
 #define XOR(R,A,B) for(int it = 0; it < Nb; it++) R[it] = A[it]^B[it]
 
-unsigned char ** keyScheduleCreation(void);
+unsigned char **keyScheduleCreation(void);
 void keyExpansion(unsigned char *key, unsigned char **word);
-unsigned char * word(unsigned char w0, unsigned char w1, unsigned char w2, unsigned char w3);
-unsigned char * rotWord(unsigned char *w);
-unsigned char * subWord(unsigned char *w);
+unsigned char *word(unsigned char w0, unsigned char w1, unsigned char w2, unsigned char w3);
+unsigned char *rotWord(unsigned char *w);
+unsigned char *subWord(unsigned char *w);
+unsigned char *Cipher(unsigned char *in, unsigned char **w);
+void subBytes(unsigned char **state);
+void shiftRows(unsigned char **state);
+void AddRoundKey(unsigned char **state, unsigned char **w, short index);
+unsigned char **StateCreation(unsigned char *in);
